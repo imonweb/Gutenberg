cb511369d475152bc66be3b71f2d3a4913f06794
Imon <contact@imonweb.co.uk>
2023-11-07 10:25:07 +0000

    YouTube Gutenberg Block

---
+++ plugins/gutenberg/build/blocks/test/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'd9b6009523283a56bf27');
---
+++ plugins/testblock/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '1a761289f77823fe22f7');
